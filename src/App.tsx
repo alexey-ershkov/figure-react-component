56320adeddc63e38e024b28425a1f53c702fa2c3
alexey ershkov <ershkov.alex2000@gmail.com>
2021-05-21 19:02:57 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,23 @@ import React from 'react';
 import Figure from "./components/Figure/Figure";
 import styled from "styled-components";
 
+const SRC = "https://images.pexels.com/photos/1647214/pexels-photo-1647214.jpeg?dl&fit=crop&crop=entropy&w=640&h=1006"
+
 const StyledApp = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
-  
   width: 75vw;
-  height: 25vh;
-  
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
+  transform: translate(-50%, -50%);
 `
 
 function App() {
     return (
         <StyledApp>
             <Figure
-                src="https://images.pexels.com/photos/1647214/pexels-photo-1647214.jpeg?dl&fit=crop&crop=entropy&w=640&h=1006"
-                arWidth={5}
-                arHeight={3}
-            >
-                Автор: <a href="https://www.pexels.com/@simonmigaj">Simon Migaj</a>
-
-            </Figure>
+                src={SRC}
+                arWidth={16}
+                arHeight={9}/>
         </StyledApp>
     );
 }
